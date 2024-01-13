05bec81303464a07139d328afcca54cafa3010ef
shaco-star <sherifm846@gmail.com>
2024-01-13 11:18:44 -0800

    commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import logging
 import re
 from typing import List
 
@@ -10,3 +11,23 @@ def filter_datum(fields: List[str], redaction: str,
         [f"(?P<{field}>{field}=[^{separator}]*)" for field in fields]))
     return pattern.sub(
         lambda match: match.group().split('=')[0] + '=' + redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        for field in self.fields:
+            message = re.sub(f"{field}=.*?{self.SEPARATOR}",
+                             f"{field}={self.REDACTION}{self.SEPARATOR}",
+                             message)
+        return message
