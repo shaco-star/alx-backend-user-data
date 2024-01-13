5fd5507c6d12941ffcfe73a2a14d397994c66334
shaco-star <sherifm846@gmail.com>
2024-01-13 10:45:15 -0800

    commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,10 @@
 import re
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    pattern = separator.join(
-        [f"{field}=.*?(?={separator}|$)" for field in fields])
-    return re.sub(
-        pattern, lambda match: match.group().replace(
-            match.group().split('=')[1], redaction), message)
+    pattern = re.compile('|'.join(
+        [f"(?P<{field}>{field}=[^{separator}]*)" for field in fields]))
+    return pattern.sub(
+        lambda match: match.group().split('=')[0] + '=' + redaction, message)
